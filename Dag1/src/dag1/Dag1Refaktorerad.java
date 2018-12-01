8324b11a5466311d1745d54e4a2b498da5b245a9
Peter Lidman <Peter Lidman@37.123.132.62>
2018-12-01 13:09:25 +0100

    Refaktorerade efter att ha kikat på Råbergiansk kod XD

---
+++ Dag1/src/dag1/Dag1Refaktorerad.java
@@ -0,0 +1,53 @@
+package dag1;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Dag1Refaktorerad {
+	static List<Integer> frekvensJusteringar = new ArrayList<Integer>();
+
+	public static void main(String[] args) throws IOException {
+		String str;
+		FileInputStream in = new FileInputStream("C:\\git\\AoC2018\\Dag1\\src\\dag1\\input.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		while ((str = br.readLine()) != null) {
+			frekvensJusteringar.add(Integer.parseInt(str));
+		}
+		br.close();
+		del1();
+		del2();
+	}
+
+	public static void del1() {
+		int sum = 0;
+
+		for (Integer frekvens : frekvensJusteringar) {
+			sum += frekvens;
+		}
+		System.out.println("Frekvens: " + sum);
+	}
+
+	public static void del2() {
+		int sum = 0;
+		Set<Integer> frekvenser = new HashSet<>();
+		boolean found = false;
+		frekvenser.add(0);
+
+		do {
+			for (Integer frekvens : frekvensJusteringar) {
+				sum += frekvens;
+				if (found = !frekvenser.add(sum)) {
+					break;
+				}
+			}
+		} while (!found);
+		System.out.println("Första dubbletten: " + sum);
+	}
+
+}
