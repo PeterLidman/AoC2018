ddc3e2dfd7aff1ad3bda114e2b69c330643f6966
Peter Lidman <Peter Lidman@37.123.132.62>
2018-12-02 10:39:17 +0100

    Lite refaktoring i lugn och ro

--- Dag2/src/dag2/Dag2.java
+++ Dag2/src/dag2/Dag2.java
@@ -9,12 +9,11 @@ import java.util.List;
 
 public class Dag2 {
 	static List<String> paketId = new ArrayList<String>();
-	static int twice = 0;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(final String[] args) throws IOException {
 		String str;
-		FileInputStream in = new FileInputStream("C:\\git\\AoC2018\\Dag2\\src\\dag2\\input.txt");
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		final FileInputStream in = new FileInputStream("C:\\git\\AoC2018\\Dag2\\src\\dag2\\input.txt");
+		final BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		while ((str = br.readLine()) != null) {
 			paketId.add((str));
 		}
@@ -26,15 +25,16 @@ public class Dag2 {
 	public static void del1() {
 		int two = 0;
 		int three = 0;
-		for (String paket : paketId) {
-			if (isNooInId(paket, 2)) {
+		
+		for (final String paket : paketId) {
+			if (isAnyCharRepeatedNrTimesInString(paket, 2)) {
 				two++;
 			}
-			if (isNooInId(paket, 3)) {
+			if (isAnyCharRepeatedNrTimesInString(paket, 3)) {
 				three++;
 			}
 		}
-		System.out.println("checksum: " + (two * three));
+		System.out.println("Checksum: " + (two * three));
 	}
 
 	public static void del2() {
@@ -43,14 +43,13 @@ public class Dag2 {
 			for (int j = 1; j < paketId.size(); j++) {
 				cs = commonString(paketId.get(i), paketId.get(j));
 				if (paketId.get(i).length() - cs.length() == 1) {
-					System.out.println("one diff: " + paketId.get(i) + " : " + cs);
-					break;
+					System.out.println("One diff: " + paketId.get(i) + " : " + cs);
 				}
 			}
 		}
 	}
 
-	public static String commonString(String s1, String s2) {
+	public static String commonString(final String s1, final String s2) {
 		String rs = "";
 
 		for (int i = 0; i < s1.length(); i++) {
@@ -61,7 +60,7 @@ public class Dag2 {
 		return rs;
 	}
 
-	public static String reduceString(String s) {
+	public static String reduceStringOfFirstChar(final String s) {
 		String rs = "";
 
 		for (int i = 1; i < s.length(); i++) {
@@ -72,12 +71,12 @@ public class Dag2 {
 		return rs;
 	}
 
-	public static boolean isNooInId(String s, int noo) {
+	public static boolean isAnyCharRepeatedNrTimesInString(String s, final int nr) {
 		String r = null;
 
 		while (s.length() > 1) {
-			r = reduceString(s);
-			if (s.length() - r.length() == noo) {
+			r = reduceStringOfFirstChar(s);
+			if (s.length() - r.length() == nr) {
 				return true;
 			}
 			s = r;
